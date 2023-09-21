5b064877cb92731fa6eaf1e087aba549d9a2d796
AngelaAcosta31 <angelamaria731@hotmail.com>
2023-09-21 12:02:30 -0500

    Posicion del personaje

--- VectoresHTML/src/js/3dmodels-main.js
+++ VectoresHTML/src/js/3dmodels-main.js
@@ -3,73 +3,83 @@ var scene = null,
     renderer = null,
     Floors = null,
     controls = null;
-let posX=0;
-    towerDirection = 1;//la torre de cubos se crea hacia la derecha
-const cubes = [];
-      size = 20;
-      divisions = 20;
-      light = null;
+    
 
+const size = 50,
+      //stats = null,
+    divisions = 50;
 
+//SONIDOS:Background, gano, perdio, recolectar cosas
+//IMAGEN DE DISEÑO EN PNG DE 1920X1080 DE PANTALLA DE GANAR Y OTRA DE PERDER
 
 /* Ajustar el tamaño de la ventana del navegador */
-window.addEventListener('resize', onWindowResize, false);
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
+window.addEventListener( 'resize', onWindowResize, false );
 
 function createThreeJs() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("app") });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    scene.background = new THREE.Color(0xdedede);
+    camera = new THREE.PerspectiveCamera( 
+                                    75,    // Field of view (arriba o abajo)
+                                    window.innerWidth / window.innerHeight, // Aspect Ratio 16:9 
+                                    0.1, // Near 
+                                    1000 ); // Far
+
+    renderer = new THREE.WebGLRenderer({canvas: document.getElementById("app")});
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-    camera.position.set(0, 20, 100);
+    camera.position.set(0,8,0);
     controls.update();
 
-    // Cuadrícula
-    const gridHelper = new THREE.GridHelper(size, divisions);
-    scene.add(gridHelper);
+    // Grid Helper
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add( gridHelper );
 
-    // Ejes
-    const axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
+    // Axes Helper
+    const axesHelper = new THREE.AxesHelper( 5 );
+    scene.add( axesHelper );
 
-   createCube();
-   createLights("PointLight");
+    // To call
+    createLights("AmbientLight");
+    createLights("PointLight");
     animate();
-
+    loadObjMtl("../models/OBJ_MTL/personaje/", "MarioBros.mtl", "MarioBros.obj");
+   // stats = new THREE.Stats();
 }
 
+function animate() {
+	requestAnimationFrame( animate );
+    controls.update();
+   // stats.update();
+	renderer.render( scene, camera );
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 function loadObjMtl(path, nameMTL, nameOBJ){
 
     //1.Load MTL (Texture)
-
     var mtlLoader = new THREE.MTLLoader();
     mtlLoader.setResourcePath(path);
     mtlLoader.setPath(path);
-    mtlLoader.load(nameMTL, function (material){
+    mtlLoader.load(nameMTL, function (material) {
         material.preload();
-    });
 
+        // 2. Load OBJ (Mesh)
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setPath(path);
+        objLoader.setMaterials(material);
+        objLoader.load(nameOBJ, function (object) {
+            scene.add(object);
+            object.position.x=0;
+        });
 
-    //2. Load OBJ (Mesh)
-    var objLoader = new THREE.OBJLoader();
-    objLoader.setPath(path);
-    objLoader.setMaterials(material);
-    objLoader.load(nameOBJ, function (object){
-        scene.add(object);
     });
 
 }
+
